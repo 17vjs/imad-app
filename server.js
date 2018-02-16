46a6345bc9923e5b89110685cf1b49c7ab2a47f9
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-16 23:58:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,15 +5,51 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+  title:'article-one',
+  content:'this is article one.<br>date:5/2/18'
+  };
+var articletwo={
+  title:'article-two',
+  content:`this is article two.<br>date:15/2/18`
+  };
+ 
+function createtemplate(data){
+var title=data.title;
+var content=data.content;
+ var htmltemplate=`
+ <html>
+<head>
+<title>
+${title}
+</title>
+        
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+    
+<body>
+<div class='container'>
+            
+<p>
+${content}
+                        
+</p>    
+            
+</div>
+</body>
+</html>`;
+
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','article-one.html'));
+  res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','article-two.html'));
+   res.send(createtemplate(articletwo));
 });
 
 app.get('/ui/style.css', function (req, res) {
