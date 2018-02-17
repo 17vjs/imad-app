cfb5014813dfcd780f65f85e91cd34fbb2b7d631
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-17 19:45:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,66 +4,74 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+
+
 var articles = {
-'article-one':{
-  title:'ARTICLE-ONE',
-  content:'this is article one.<br>date:5/2/18<br>  <a href="/article-two">article-two</a><br>  <a href="/">home</a>'
-  },
-'article-two':{
-  title:'ARTICLE-TWO',
-  content:`this is article two.<br>date:15/2/18<br>  <a href="/article-one">article-one</a><br>  <a href="/">home</a>`
-  }
-};
+                'article-one':{
+                              title:'ARTICLE-ONE',
+                              content:'this is article one.<br>date:5/2/18<br>  <a href="/article-two">article-two</a><br>  <a href="/">home</a>'
+                              },
+                'article-two':{
+                              title:'ARTICLE-TWO',
+                              content:`this is article two.<br>date:15/2/18<br>  <a href="/article-one">article-one</a><br>  <a href="/">home</a>`
+                              }
+                };
+
 function createtemplate(data){
-var title=data.title;
-var content=data.content;
- var htmltemplate=`
- <html>
-<head>
-<title>
-${title}
-</title>
-        
-<link href="/ui/style.css" rel="stylesheet" />
-</head>
-    
-<body>
-<div class='container'>
-            
-<p>
-${content}
-                        
-</p>    
-            
-</div>
-<div>
-  
-    
-       
-        
-</div>
-</body>
-</html>`;
+                                var title=data.title;
+                                var content=data.content;
+                                var htmltemplate=`  <html>
+                                                    <head>
+                                                    <title>
+                                                    ${title}
+                                                    </title>
+                                                            
+                                                    <link href="/ui/style.css" rel="stylesheet" />
+                                                    </head>
+                                                        
+                                                    <body>
+                                                    <div class='container'>
+                                                                
+                                                    <p>
+                                                    ${content}
+                                                                            
+                                                    </p>    
+                                                                
+                                                    </div>
+                                                    <div>
+                                                      
+                                                        
+                                                           
+                                                            
+                                                    </div>
+                                                    </body>
+                                                    </html>`;
+                                                    
+                                return htmltemplate;
+                             }
 
-return htmltemplate;
-}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','index.html'));
-});
+                                      res.sendFile(path.join(__dirname,'ui','index.html'));
+                                 });
+                                 
+                                 
+                                 
 app.get('/:articlename', function (req, res) {
- var articlename=req.params.articlename;
-  res.send(createtemplate(articles[articlename]));
-});
-
+                                                 var articlename=req.params.articlename;
+                                                  res.send(createtemplate(articles[articlename]));
+                                              });
 
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+                                                  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+                                             });
+
+
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+                                                  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+                                            });
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
