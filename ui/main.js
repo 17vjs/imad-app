4aa0159ef2bee4dccdf80536553890a774ff8931
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-19 22:09:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ alert("hi ");
 var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
-{ if (marginLeft<=500)
+{ if (marginLeft<=1000)
     {marginLeft = marginLeft + 1 ;
 img.style.marginLeft=marginLeft + 'px';}
 else
