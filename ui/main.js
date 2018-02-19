3dd824f00b0c6dc188e5e48b56f616d902d2a697
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-19 23:38:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ console.log('Loaded!');
 
 
 var button=document.getElementById('counter');
-var counter1=0;
-button.onclick = function c() {
-    counter1 = counter1 + 1;
+var counter=0;
+button.onclick = function () {
+    counter = counter + 1;
     var span=document.getElementById('count');
-    span.innerHTML = counter1.toString();
+    span.innerHTML = counter.toString();
 };
