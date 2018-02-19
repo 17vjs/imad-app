1904c395d5d98db8d6eb415afbd6c828b4547a0f
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-19 22:12:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,15 +4,17 @@ alert("hi ");
 var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
-{ if (marginLeft<=1000)
-    {marginLeft = marginLeft + 1 ;
-img.style.marginLeft=marginLeft + 'px';}
-else
-{marginLeft=0;
-img.style.marginLeft=marginLeft + 'px';}
+{ 
+    marginLeft = marginLeft + 1 ;
+img.style.marginLeft=marginLeft + 'px';
+
 }
 img.onclick= function c() {
     var interval = setInterval (moveRight , 1);
+    if(marginLeft==100)
+    {
+        exit(0);
+    }
    
 
 };
