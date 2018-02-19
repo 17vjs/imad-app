8d3da7eaafaa9abcb0416dca22ca4524a6d096e2
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-19 22:00:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var marginLeft=0;
 function moveRight()
 {
     marginLeft = marginLeft + 10 ;
-img.style.marginLeft=marginleft + 'px';
+img.style.marginLeft=marginLeft + 'px';
 
 }
 img.onclick= function c() {
