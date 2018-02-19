fc4bf7db4fe49b3937e39dc8b8c01b287bdd5dda
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-20 00:18:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,15 @@ button.onclick = function () {
     request.send(null);
   
 };
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('submit');
+submit.onclick=function (){
+   var names=['name1','name2','name3','name4'];
+   var list='';
+   for (var i=0;i<names.length;i++)
+   {list+='<li>'+names+'<li>';
+   }
+   var u1=document.getElementById('namelist');
+   u1.innerHTML=list;
+};
