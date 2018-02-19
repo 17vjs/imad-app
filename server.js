692fe5379bbf74f103691b0f3099c135555557e7
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-20 00:28:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,6 +54,13 @@ function createtemplate(data){
 app.get('/', function (req, res) {
                                       res.sendFile(path.join(__dirname,'ui','index.html'));
                                  });
+var names=[];
+app.get('/submit/:name', function (req, res) {
+    var name=req.params.name;
+    names.push(name);
+    
+                                      res.send(JSON.stringify(names));
+                                 });
     var counter=0;                             
  app.get('/counter', function (req, res) {
      counter=counter+1;
