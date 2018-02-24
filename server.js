647f843b119a2df638894d913e10994e5d9b3557
17vjs <36515090+17vjs@users.noreply.github.com>
2018-02-24 19:58:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ pool.query('SELECT * FROM users',function(err,result){
                                  
 app.get('/articles/:articlename', function (req, res) {
                                                  
-                                                 pool.query("SELECT * FROM article WHERE title=''" +req.params.articlename+ "'",function(err,result){
+                                                 pool.query("SELECT * FROM article WHERE title='" +req.params.articlename+ "'",function(err,result){
     if(err){
         res.status(500).send(err.toString());
     }
